b2e39dfce3608ae137b49c116a912c04a072ac26
zach <zacharyle25@gmail.com>
2026-03-12 22:04:53 -0400

    Removed unneded header file and func prototypes

--- life.h
+++ life.h
@@ -1,12 +1,18 @@
-#include "common.h"
+#include <raylib.h>
+
+//#define DEBUG
+
+#define SCREEN_HEIGHT 800
+#define SCREEN_WIDTH  800
+
+#define MAX_ROW 10
+#define MAX_COL MAX_ROW
+#define CELL_SIZE (int)(SCREEN_HEIGHT / MAX_ROW)
 
 typedef enum state { DEAD, ALIVE } State;
 typedef State Grid[MAX_ROW + 2][MAX_COL + 2];
 
 void copy_map(Grid map, Grid new_map);
 void reset_map(Grid map);
-void set_neighbor(Grid map, int row, int col);
 int neighbor_count(Grid map, int row, int col);
-
-void draw_map(Grid map);
-void draw_neighbor(int row, int col);
+void update_map(Grid map);
